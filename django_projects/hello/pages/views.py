40469badeba9060ee206385338a4dd04f0883173
owari-taro <taro.biwajima@gmail.com>
2021-10-21 17:46:49 +0900

    first commit with docker

--- django_projects/hello/pages/views.py
+++ django_projects/hello/pages/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page_view(request):
-    return HttpResponse("hello world!")
+    """
+    [summary]
+
+    Parameters
+    ----------
+    request : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """    
+    return HttpResponse("hello world!\nare you ok?if you are fine tell me about it")
